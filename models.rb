96abb8714b9fc457b6bc91f325aca6125be1ea37
William Lee <wlee@mochify.com>
2011-09-30 10:56:59 -0400

    Adding preliminary models file - right now it doesn't work when I want a sqlite3 database...

---
+++ models.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/env ruby
+#
+require 'datamapper'
+
+
+class Post
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :title,      String, :required => true
+  property :body,       Text, :required => true
+  property :summary,    Text
+  property :createdAt,  EpochTime
+  property :guid,       UUID
+
+  has n, :comments
+  has n, :tags
+  belongs_to :user
+end
+
+class Comment
+  include DataMapper::Resource
+
+  property :id,   Serial
+  property :postAuthor, String
+  property :comment, Text
+  property :guid, UUID
+
+  belongs_to :post
+end
+
+class Tag
+  include DataMapper::Resource
+
+  property :id,   Serial
+  property :name, String, :required => true
+  property :description, Text
+end
+
+class User
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :userName, String, :required => true
+  property :email, String, :required => true
+  property :firstName, String
+  property :lastName, String
+  property :password, BCryptHash
+  property :lastLogin, EpochTime
+  property :active, Boolean, :default => true
+  property :guid, UUID
+
+  has n, :roles, :through => Resource
+end
+
+class Role
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :roleId, Integer, :required => true
+  property :guid, UUID
+  property :name, String, :required => true
+
+  has n, :users, :through => Resource
+end
+
+DataMapper.setup(:default, 'sqlite3://test.db')
+DataMapper.finalize
+#DataMapper.auto_upgrade!
+#DataMapper.auto_migrate!
+User.auto_migrate!
+Role.auto_migrate!
